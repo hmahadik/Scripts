9355df16d077860fdabbd727424f7f3c53ffe4f4
Harshad Mahadik <harshad@arcturusnetworks.com>
2020-09-28 12:45:34 -0400

    Update mixed dpi scaling script with commands for DVI left of 4K

--- mixed_dpi_scaling.sh
+++ mixed_dpi_scaling.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -x
-echo "setting 27 inch 4K monitor"
+# 1080p HDMI right of 4K DisplayPort-0
 xrandr --output DisplayPort-0 --mode 3840x2160 --scale "1.5x1.5" --rate 60 --primary
-echo "setting 24 inch 1080p monitor"
 xrandr --output HDMI-A-0 --mode 1920x1080 --scale 2x2 --pos 5760x0
-echo "setting global scaling to 2x" 
 gsettings set org.gnome.desktop.interface scaling-factor 2
+
+# 1680x1050 DVI left of 4K DP-0
+# xrandr --output DVI-D-0 --mode 1680x1050 --scale 2x2 --pos 0x0
+# xrandr --output DP-0 --mode 3840x2160 --scale "1.25x1.25" --rate 60 --primary --pos 3360x0
+# gsettings set org.gnome.desktop.interface scaling-factor 2
